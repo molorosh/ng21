f5e2fafa6363e019777761a0fc7a0cb86e92382f
Howard G Ricketts <molorosh@gmail.com>
2025-11-27 22:15:35 +0000

    tweaked title

--- ng-twenty-one/src/app/app.spec.ts
+++ ng-twenty-one/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-twenty-one');
+    expect(compiled.querySelector('h1')?.textContent).toContain('ng-twenty-one');
   });
 });
