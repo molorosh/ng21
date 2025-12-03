a1efa2bb84678015ad33c66574f9872053fb38f5
Howard G Ricketts <molorosh@gmail.com>
2025-12-03 23:26:28 +0000

    a slightly working getAll group tasks button and UX - ngOnInit not loading task groups though :-(

--- ng-twenty-one/src/app/ux/components/my-work/my-work.ts
+++ ng-twenty-one/src/app/ux/components/my-work/my-work.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit, WritableSignal, ChangeDetectionStrategy } from '@angular/core';
 import { TaskGroupRepository } from '../../../domain/task-group-repository.interface';
 import { TaskGroupRepositoryService } from '../../../services/task-group-repository.service';
 import { TaskGroup } from '../../../domain/task-group.class';
@@ -9,12 +9,16 @@ import { EntityStatus } from '../../../ddd/entity-status.enum';
   imports: [],
   templateUrl: './my-work.html',
   styleUrl: './my-work.less',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MyWork {
+export class MyWork implements OnInit {
   // I'm still using the interface here - but supplying the concrete implementation via inject/DI
   repo : TaskGroupRepository = inject(TaskGroupRepositoryService)
   thePk = signal(-21);
   theTitle = signal('Group A');
+
+  // the list of task groups
+  theGroups = signal<TaskGroup[]>([]);
 
   public create(): void {
     console.log(" create clicked ");
@@ -25,7 +29,12 @@ export class MyWork {
   }
 
   public getAll(): void {
-    console.log(" getAll clicked ");
-    this.repo.getAll().then(i => console.log("items", i));
+    console.log(" getAll() ");
+    this.repo.getAll().then(i => this.theGroups.set(i));
+  }
+
+  ngOnInit(){
+    console.log(" ngOnInit() ");
+    this.repo.getAll().then(i => this.theGroups.set(i));
   }
 }
